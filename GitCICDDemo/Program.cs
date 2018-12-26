67b0b1b9a76c5b7045b86db07b5c7a691b02c7cd
Cvetan Stoimenov <cst@succcessit.io>
2018-12-26 13:15:25 +0100

    Added config

--- GitCICDDemo/Program.cs
+++ GitCICDDemo/Program.cs
@@ -14,7 +14,11 @@ namespace GitCICDDemo
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .Build()
+                .Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
